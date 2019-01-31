3f05a8f93a0f2aac3e5173fb12ba28922c30f3f0
Corey Walsh <matthias61984@hotmail.com>
2019-01-30 21:11:14 -0800

    index 0 in

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 import Discover from "./pages/Discover";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper"
+import Favorites from "./pages/Favorites";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
         <Wrapper>
           <Route exact path="/" component={Login} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/home" component={Home} />
           <Route exact path="/discover" component={Discover} />
+          <Route exact path="/favorites" component={Favorites} />
         </Wrapper>
       </div>
     </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,12 @@ import Jumbotron from "../components/Jumbotron";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Navbar from "../components/Navbar";
 
-function Login() {
+function Home() {
   return (
     <div>
+      <Navbar/>
       <Jumbotron backgroundImage="https://images.unsplash.com/photo-1512805147242-c3e79caf64bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80">
         <h1>TableIt</h1>
         <h2>Find dinner tonight... and tomorrow night.</h2>
@@ -29,4 +31,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Home;

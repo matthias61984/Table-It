914de2931dbeb4093ab7f80bd7f41def1836f0fc
Doctor Mew <trc379@gmail.com>
2019-02-02 12:07:23 -0800

    Discover Functionality

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.use(apiRoutes);
+app.use("/api", apiRoutes);
 
 app.use("*", function (req, res) {
     return res.sendFile(path.join(__dirname, "./src/public/index.html"));

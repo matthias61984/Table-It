ba50f67ed6115c9e1d098bb4e0f03052581a1a8b
Doctor Mew <trc379@gmail.com>
2019-02-02 14:04:50 -0800

    Vanillux

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("/build"));
+    app.use(express.static(path.join(__dirname,"./build")));
 }
 
 
+app.use(express.static(path.join(__dirname,"./build")));
 app.use("/api", apiRoutes);
 
 app.use("*", function (req, res) {
